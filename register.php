50e8ea357a45fb81cc3956b38ba75586afe2071f
quality-ctce <nurd00915@gmail.com>
2024-11-17 18:29:10 +0800

    Add files via upload

---
+++ register.php
@@ -0,0 +1,15 @@
+<?php
+require 'connect.php';
+$fullname = $_GET ["fullname"];
+$username = $_GET["username"];
+$email = $_GET["email"];
+$password = $_GET["password"];
+
+$query_sql = "INSERT INTO tbl_users (fullname, username, email, password) VALUE ('$fullname', '$username', '$email', '$password')";
+
+if (mysqli_query($conn, $query_sql)) {
+    header("Location: index.html");
+}
+else {
+    echo "Pendaftaran Gagal : " . mysqli_error($conn);
+}
